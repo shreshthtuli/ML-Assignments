588510d5de136ee363ad0cca1ec83cf454060a98
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2019-01-25 22:57:20 +0530

    Updated code

--- Linear Regression/Linear_Regression.py
+++ Linear Regression/Linear_Regression.py
@@ -17,101 +17,115 @@ from mpl_toolkits.mplot3d import Axes3D
 import time
 from random import shuffle, random
 
-# Global variables
+# Variables used in learing algorithm
 iteration = 0
 LearningRate = 0.1
 Epsilon = 1e-30
-Saved_Theta = []
+history = []
 Saved_J = []
 
-''' This provides the analytical solution '''
+# Data variables
+X_orig = []
+Y = []
+X = [] # Normalized X with X0 as 1
+
+# Read Data from file and return numpy array
+def read(filename):
+	return np.matrix([map(float, line.strip().split()) for line in open(filename)])
+
+# Return Hypothesis at X and Theta
+def hypothesis(X, Theta):
+	return X * Theta
+
+# Return Hypothesis for a particular example X(i)
+def hypothesis_particular(X, Theta, i):
+	return X[i] * Theta
+
+# Hypothesis function for a particular X
+def hypothesis_x(Theta, x):
+	return (Theta[1] * x) + Theta[0]
+
+# Determine Cost function (J) for a given X and Y parameterized by Theta
+def J(X, Y, Theta):
+	return (np.linalg.norm(Y - hypothesis(X, Theta))**2 / (2*X.shape[0])).item(0)
+
+# Determine gradient of cost function
+def gradJ(X, Y, Theta):
+	return X.T * (hypothesis(X, Theta) - Y) / X.shape[0]
+
+# Determine gradient of cost function for a particular training example
+def gradJ_sgd(X, Y, Theta, i):
+	return X[i].T * (hypothesis_particular(X, Theta, i) - Y[i]) 
+
+# Returns the value of J(theta)
+def J_plot(Theta_0, Theta_1):
+	Theta = np.matrix([[Theta_0],[Theta_1]])
+	return ((Y - X * Theta).T * (Y - X * Theta) / (2*X.shape[0])).item(0)
+
+# Returns normalised numpy array
+def normalise(X):
+	X_mean = np.mean(X_orig, axis=0)
+	X_std = np.std(X_orig, axis=0)
+	return (X - X_mean)/X_std
+
+# Analytical Solution for linear regression 
 def analytical_solution(X, Y):
     return np.linalg.inv(X.T * X) * X.T * Y
 
-''' To Read the X values '''
-def Xread():
-	return np.matrix([map(float, line.strip().split()) for line in open('linearX.csv')])
-
-''' To Read the Y values '''
-def Yread():
-	return np.matrix([[float(line.strip())] for line in open('linearY.csv')])
- 
-''' To create a theta vector based on dimension of input '''
-def initialize_theta(x):
-	return np.matrix([[float(0)]] * (x))
-
-''' Returns the value of J(theta) '''
-def create_J(X, Y, Theta):
-	return ((Y - X * Theta).T * (Y - X * Theta) / (2*X.shape[0])).item(0)
-
-''' Returns the gradient of J(theta) '''
-def create_gradJ(X, Y, Theta):
-	return X.T * (X * Theta - Y) / X.shape[0]
-
-''' Returns the gradient of J(theta) '''
-def create_gradJ_sgd(X, Y, Theta, i):
-	return X[i].T * ((X[i] * Theta) - Y[i]) 
-
-''' Returns normalised data '''
-def normalise(X):
-	var = np.linalg.norm(X)
-	mean = np.linalg.mean(X)
-	return (X - mean) / var
-
-''' The Gradient Descent Algorithm '''
+# Batch Gradient Descent Algorithm
 def gradient_descent(X, Y):
 	global iteration
 	global LearningRate
-	global Saved_Theta
-	t = 0
-	Theta = initialize_theta(X.shape[1])
-	J = create_J(X, Y, Theta)
-	Saved_J.append(J)
-	Saved_Theta.append([item for sublist in Theta.tolist() for item in sublist] + [J])
+	global history 
+	t = 0 # time variable
+	Theta = np.matrix([[float(0)]] * (X.shape[1])) # Initialise theta based on input size
+	j = J(X, Y, Theta)
+	Saved_J.append(j)
+	history.append([item for sublist in Theta.tolist() for item in sublist])
 	while(True):
 		# print LearningRate
 		iteration += 1;
 		start_time = time.time()
-		gradJ = create_gradJ(X, Y, Theta)
-		newTheta = Theta - LearningRate * gradJ
-		newJ = create_J(X, Y, newTheta)
+		grad = gradJ(X, Y, Theta)
+		newTheta = Theta - LearningRate * grad
+		newJ = J(X, Y, newTheta)
 		Theta = newTheta
 		t += (time.time() - start_time)
-		if J - newJ < Epsilon: 	# Value has converged
+		if j - newJ < Epsilon: 	# Value has converged
 			break		
-		J = newJ
-		Saved_Theta.append([item for sublist in Theta.tolist() for item in sublist] + [J])
-		Saved_J.append(J)
+		j = newJ
+		history.append([item for sublist in Theta.tolist() for item in sublist])
+		Saved_J.append(j)
 	print("Total time : "),
 	print(t)
 	return Theta
 
-''' The Stochastic Gradient Descent Algorithm '''
+# Stochastic Gradient Descent Algorithm
 def stochastic_gradient_descent(X, Y):
 	global iteration
 	global LearningRate
-	global Saved_Theta
-	t = 0
-	Theta = initialize_theta(X.shape[1])
-	J = create_J(X, Y, Theta)
-	Saved_J.append(J)
-	Saved_Theta.append([item for sublist in Theta.tolist() for item in sublist] + [J])
+	global history
+	t = 0 # time variable
+	Theta = np.matrix([[float(0)]] * (X.shape[1])) # Initialise theta based on input size
+	j = J(X, Y, Theta)
+	Saved_J.append(j)
+	history.append([item for sublist in Theta.tolist() for item in sublist])
 	while(True):
 		newTheta = Theta
-		oldJ = J
+		oldJ = j
 
 		shuffled_list = range(1, X.shape[0])
-		shuffle(shuffled_list)
+		shuffle(shuffled_list) # Shuffle order of examples after each epoch
 		for x in shuffled_list:
 			start_time = time.time()
-			gradJ = create_gradJ_sgd(X, Y, Theta, x)
-			newTheta = Theta - LearningRate * gradJ
-			newJ = create_J(X, Y, newTheta)
+			grad = gradJ_sgd(X, Y, Theta, x)
+			newTheta = Theta - LearningRate * grad
+			newJ = J(X, Y, newTheta)
 			Theta = newTheta
 			t += (time.time() - start_time)
-			Saved_Theta.append([item for sublist in Theta.tolist() for item in sublist] + [J])
-			Saved_J.append(J)
-			J = newJ
+			history.append([item for sublist in Theta.tolist() for item in sublist])
+			Saved_J.append(j)
+			j = newJ
 			iteration += 1;
 
 		if oldJ - newJ < Epsilon: 	# Value has converged
@@ -120,39 +134,23 @@ def stochastic_gradient_descent(X, Y):
 	print(t)			
 	return Theta
 
-''' Equation of the hypothesis function '''
-def linear(Theta, x):
-	return (Theta.item(1) * x) + Theta.item(0)
+# Read Data
+X_orig = read('linearX.csv')
+Y = read('linearY.csv')
 
-def linear2(Theta, x):
-	return (Theta[1] * x) + Theta[0]
-
-### 3D plot of the J(theta) function ###
-# Returns the value of J(theta)
-def create_J_plot(Theta_0, Theta_1):
-	Theta = np.matrix([[Theta_0],[Theta_1]])
-	return ((Y - X * Theta).T * (Y - X * Theta) / (2*X.shape[0])).item(0)
-
-# Read input values
-X_orig = Xread()
-Y = Yread()
-
+# Prints size of training set
 print ("Number of examples : %s" % X_orig.shape[0])
 
 # Take learning rate from user
 print("Enter Learning rate : "),
 LearningRate = input()
 
-# Enter Gradient Descent Algorithm
+# Take which Gradient Descent Algorithm to use from user
 print("Enter 0 for BGD and 1 for SGD : "),
 option = input()
 
-# Normalize
-X = X_orig
-X_mean = np.mean(X_orig, axis=0)
-X_std = np.std(X_orig, axis=0)
-X = (X - X_mean)/X_std
-X = np.c_[np.ones((X.shape[0], 1)), X]
+# Normalize X values
+X = np.c_[np.ones((X_orig.shape[0], 1)), normalise(X_orig)]
 
 # Perform Gradient Descent
 FinalTheta = []
@@ -161,35 +159,32 @@ if option == 1:
 else:
 	FinalTheta = gradient_descent(X, Y)
 
-# Print Output
+# Print results
+print 'Iterations used = ', iteration
 print 'Analytical Solution\n', analytical_solution(X,Y)
 print 'Gradient Decent Solution\n', FinalTheta
-print 'Iterations used = ', iteration
 
-### 2D plot of the hypothesis function ###
-X_plot = [item[1] for item in X.tolist()]
+# Create plots
 Y_plot = [item[0] for item in Y.tolist()]
-x = np.array(X_plot)
+x = np.array([item[1] for item in X.tolist()])
 
 fig = plt.figure(figsize=(30, 30))
 
-A = []; B = []; C = []
-theta_0_plot = np.linspace(-0.25, 2, 100)
-theta_1_plot = np.linspace(-1, 1, 100)
-theta_0_plot, theta_1_plot = np.meshgrid(theta_0_plot, theta_1_plot)
-Z = np.vectorize(create_J_plot)(theta_0_plot, theta_1_plot)
+A = []; B = [];
+theta0 = np.linspace(-0.25, 2, 100)
+theta1 = np.linspace(-1, 1, 100)
+theta0, theta1 = np.meshgrid(theta0, theta1)
+Z = np.vectorize(J_plot)(theta0, theta1)
 ax3 = fig.add_subplot(2, 2, 3, projection='3d')
-ax3.plot_surface(theta_0_plot, theta_1_plot, Z, rstride=1, cstride=1, alpha=0.3, linewidth=0.1, cmap=cm.coolwarm)
+ax3.plot_surface(theta0, theta1, Z, rstride=1, cstride=1, alpha=0.3, linewidth=0.1, cmap=cm.coolwarm)
 ax3.set_zlim(min(Saved_J), max(Saved_J))
 
-
-# Plot
+# Plot for each iteration
 for index in range(iteration+1):
-	line = Saved_Theta[index]
-	# print line[0],line[1],line[2]
+	line = history[index]
 	plt.subplot(2, 2, 1)	
 	plt.plot(X_orig, Y_plot, 'ro')
-	ln, = plt.plot(X_orig, linear2(line, x))
+	ln, = plt.plot(X_orig, hypothesis_x(line, x))
 	plt.axis([min(X_orig)-0.1*np.std(X_orig), max(X_orig)+0.1*np.std(X_orig), min(Y_plot)-0.1*np.std(Y_plot), max(Y_plot)+0.1*np.std(Y_plot)])
 	plt.ylabel('Density')
 	plt.xlabel('Acidity')
@@ -202,23 +197,23 @@ for index in range(iteration+1):
 	plt.title('Cost with time')
 	index = index + 1
 
-	A.append(line[0]); B.append(line[1]); C.append(line[2])
-	wframe, = ax3.plot(A, B, C)
-	point = ax3.plot([line[0]],[line[1]],[line[2]], 'r.')
+	A.append(line[0]); B.append(line[1]);
+	wireframe, = ax3.plot(A, B, Saved_J[0:index:1])
+	point = ax3.plot([line[0]],[line[1]],[Saved_J[index-1]], 'r.')
 
 	plt.subplot(2, 2, 4)
-	CS = plt.contour(theta_0_plot, theta_1_plot, Z)
+	CS = plt.contour(theta0, theta1, Z)
 	plt.title('Contour Plot Showing Gradient Descent')
 	point = plt.plot([line[0]],[line[1]], 'ro')
 	point = plt.plot(A,B)
 
 	plt.pause(0.0001)
 
-	if index == iteration:
+	if index == iteration-1:
 		break
 	ln.remove()
 	ln2.remove()
-	wframe.remove()
+	wireframe.remove()
 
 gd = ''
 if option == 0:
