8dfd13d14322377b8eba518807f34f47d992be94
Shreshth Tuli <shreshthtuli@gmail.com>
2019-03-07 21:51:36 +0530

    Update Naive_Bayes.py

--- Naive Bayes/Naive_Bayes.py
+++ Naive Bayes/Naive_Bayes.py
@@ -17,9 +17,15 @@ from mpl_toolkits.mplot3d import Axes3D
 import time
 from random import shuffle, random
 from utils import *
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import classification_report
+import nltk
 
 # Hyper parameters
-tokenize_type = 1
+tokenize_type = 2
 
 # Global constants
 categories = 5
@@ -35,12 +41,20 @@ def tokenize(str):
         return str.strip().split()
     elif tokenize_type == 2:
         return getStemmedDocuments(str)
+    elif tokenize_type == 3:
+        a = getStemmedDocuments(str)
+        a.extend(list(nltk.bigrams(str.split())))
+        return a
+    elif tokenize_type == 4:
+        a = getStemmedDocuments(str)
+        a.extend(a[:10])
+        return a
 
 def generate_vocab():
     seen = set()
     output = []
     inputs = []
-    counter = 0; limit = 100000
+    counter = 0; limit = 10000
     for value in json_reader("train.json"):
         inputs.append(value)
         for word in tokenize(value["text"]):
@@ -48,7 +62,7 @@ def generate_vocab():
                 output.append(word)
                 seen.add(word)
         counter+= 1
-        # if counter == limit: break
+        if counter == limit: break
     return (dict(zip(output, np.arange(0, len(output), 1))), inputs)
 
 def train(vocab, inputs):
@@ -94,14 +108,32 @@ def classify(text):
 def test(filename, Phi, Theta):
     correct = 0
     count = 0
+    actual_ys = []; predicted_ys = []
     for view in json_reader(filename):
         count += 1
         actual_y = int(view["stars"])
         precited_y = classify(view["text"])
+        actual_ys.append(actual_y); predicted_ys.append(precited_y)
         if(actual_y == precited_y):
             correct += 1
-        # if count > 10000: break
-    return float(correct)/float(count)
+        if count > 10000: break
+    return float(correct)/float(count), actual_ys, predicted_ys
+
+def plot_confusion_matrix(cm, classes):
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]), yticks=np.arange(cm.shape[0]),
+           xticklabels=classes, yticklabels=classes, title='Confusion matrix',
+           ylabel='True label', xlabel='Predicted label')
+
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], 'd'), ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
 
 
 vocab, inputs = generate_vocab()
@@ -115,4 +147,14 @@ print(Theta)
 print(Denom)
 
 print ("Accuracy = "), 
-print (test("test.json", Phi, Theta))
+accuracy, actual_ys, predicted_ys = test("test.json", Phi, Theta)
+print (accuracy)
+
+cm = confusion_matrix(actual_ys, predicted_ys)
+
+print(cm)
+
+# plot_confusion_matrix(cm, [1, 2, 3, 4, 5])
+# plt.show()
+
+print(classification_report(actual_ys, predicted_ys))
