9aa7dff46fce2f723032b94adfcaefbfbff53431
shreshthtuli <34661516+shreshthtuli@users.noreply.github.com>
2019-03-07 19:14:54 +0530

    Updated nb code

--- Naive Bayes/Naive_Bayes.py
+++ Naive Bayes/Naive_Bayes.py
@@ -18,17 +18,100 @@ import time
 from random import shuffle, random
 from utils import *
 
+# Hyper parameters
+tokenize_type = 1
+
+# Global constants
+categories = 5
+dicts = 0
+
+# Parameters
+Phi = [0, 0, 0, 0, 0] # For y : 1 to 5
+Theta = np.empty((dicts, categories)) # 
+
+def tokenize(str):
+    if tokenize_type == 1:
+        return str.strip().split()
+    elif tokenize_type == 2:
+        return getStemmedDocuments(str)
+
 def generate_vocab():
     seen = set()
-    values = []
     output = []
+    inputs = []
     for value in json_reader("train.json"):
-        values.extend(value["text"].strip().split())
-    for word in values:
-        if word not in seen:
-            output.append(value)
-            seen.add(value)
-    print output
-    return output
+        inputs.append(value)
+        for word in tokenize(value["text"]):
+            if word not in seen:
+                output.append(word)
+                seen.add(word)
+    return (dict(zip(output, np.arange(0, len(output), 1))), inputs)
 
-vocab = generate_vocab()
+def train(vocab, inputs):
+    # Initialization
+    Phi = [0, 0, 0, 0, 0] # For y : 1 to 5
+    Theta = np.empty((dicts, categories)) # 
+    Denom = [0, 0, 0, 0, 0]
+    M = 0 # Number of training examples
+    for i in range(dicts):
+	    for j in range(categories):
+		    Theta[i][j] = 0
+
+    for inp in inputs:
+        M += 1
+        c = int(inp["stars"])
+        Phi[c-1] += 1
+        for word in tokenize(inp["text"]):
+            k = vocab[word]
+            Theta[k-1][c-1] += 1
+            Denom[c-1] += 1
+    
+    for i in range(categories):
+        Phi[i] = Phi[i]/M
+    for k in range(dicts):
+        for c in range(categories):
+            Theta[k][c] = (Theta[k][c] + 1) / (Denom[c] + dicts)
+    
+    return Phi, Theta
+
+def theta_k_c(token, category):
+    k = 0
+    try:    
+        k = vocab[token]
+    except:
+        return 1 / dicts
+    return Theta[k][category]
+
+    
+def classify(text):
+    probs = np.array([0, 0, 0, 0, 0])
+    tokens = tokenize(text)
+    for c in range(categories):
+        for token in tokens:
+            try:
+                probs[c] += math.log(Phi[c]) + math.log(theta_k_c(token, c))
+            except:
+                print (Phi[c], theta_k_c(token, c))
+    predicted_cat = probs.argmax() + 1
+    return predicted_cat
+
+def test(filename, Phi, Theta):
+    correct = 0
+    count = 0
+    for view in json_reader(filename):
+        count += 1
+        actual_y = int(view["stars"])
+        precited_y = classify(view["text"])
+        if(actual_y == precited_y):
+            correct += 1
+    return float(correct)/float(count)
+
+
+vocab, inputs = generate_vocab()
+
+dicts = len(vocab)
+
+Phi, Theta = train(vocab, inputs)
+
+print ("Accuracy = "), 
+print (test("test.json", Phi, Theta))
