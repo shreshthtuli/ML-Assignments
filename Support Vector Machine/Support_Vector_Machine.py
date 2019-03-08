86d7835342e78a66022ec8e4847d1d6458d770e1
Shreshth Tuli <shreshthtuli@gmail.com>
2019-03-09 02:08:54 +0530

    Update Support_Vector_Machine.py

--- Support Vector Machine/Support_Vector_Machine.py
+++ Support Vector Machine/Support_Vector_Machine.py
@@ -20,11 +20,11 @@ def parseData(filename):
     Y = read[:,-1]
     return X, Y
 
-def convertLinear(X, Y, d, changeY=True):
+def convertLinear(X, Y, d, e=(d+1)%10, changeY=True):
     retY = Y[np.logical_or.reduce([Y == x for x in [d,d+1]])]
     if changeY:
         retY[retY == d] = -1
-        retY[retY == (d+1)%10] = 1
+        retY[retY == e] = 1
     retX = np.array(X[np.where(np.logical_or.reduce([Y == x for x in [d,d+1]]))[0], :])
     return retX, retY.T
 
@@ -55,9 +55,9 @@ def intercept_l(X, Y, w):
 
 def intercept_g(X, Y, a):
 	mat = np.matrix([map(float, [0]) for _ in xrange(Y.shape[0])])
-	for i in xrange(m):
+	for i in xrange(Y.shape[0]):
 		temp = 0
-		for j in xrange(m):
+		for j in xrange(Y.shape[0]):
 			temp += a[j].value*Y.item(j)*math.exp(-2.5 * np.square(norm(X[i], X[j])).item())
 		mat[i] = temp
 	return -0.5*(np.max(np.matrix(mat[Y == -1])) + np.min(np.matrix(mat[Y == 1])))
@@ -79,7 +79,7 @@ def train(X, Y, kernel_type, C=1, gamma=0.05):
             savefig(X[i].reshape(1, 784), "./sv/supportvector"+str(i)+"y"+str(Y[i])+".png")
         
     w = weight_vector(X, Y, index)
-    b = intercept_l(X, Y, w) if kernel_type == "linear" else intercept_g(X, Y, w)
+    b = intercept_l(X, Y, w) if kernel_type == "linear" else intercept_g(X, Y, alpha)
     return w, b
 
 def test(w, b, filename):
@@ -87,10 +87,43 @@ def test(w, b, filename):
     X1, Y1 = convertLinear(X1, Y1, d, False)
     correct = 0
     total = 0
-    print (w.shape, X1.shape, Y1.shape)
     for i in xrange(Y1.shape[0]):
         val = float(w*(X1[i].reshape(1, X.shape[1]).T)) + b
         clsfy = d+1 if val >= 0 else d
+        if clsfy == Y1.item(i):
+            correct += 1
+        else:
+            savefig(X1[i].reshape(1, X.shape[1]), "./wrong/wrong"+str(total)+"a"+str(int(Y1.item(i)))+"p"+str(int(clsfy))+".png")
+        total += 1
+    
+    return float(correct) / float(total)
+
+def train_multiclass(X, Y, kernel_type, C=1, gamma=0.05):
+    w = np.empty((10, 10))
+    b = np.empty((10, 10))
+    for i in range(10):
+        for j in range(10):
+            if i != j:
+                Xd, Yd = convertLinear(X, Y, i, j)
+                w[i][j], b[i][j] = train(X, Y, kernel_type, X, gamma)
+    return w, b
+
+def classify(w, b, x):
+    wins = np.zeros(10)
+    for i in range(10):
+        for j in range(10):
+            if i != j:
+                val = float(w*(x.reshape(1, 784).T)) + b
+                clsfy = j if val >= 0 else x
+                wins[clsdfy] += 1
+    return wins.argmax() + 1
+
+def test_multiclass(w, b, filename):
+    X1, Y1 = parseData(filename)
+    correct = 0
+    total = 0
+    for i in xrange(Y1.shape[0]):
+        clsfy = classify(w, b, X1[i])
         if clsfy == Y1.item(i):
             correct += 1
         else:
@@ -105,11 +138,11 @@ X, Y = parseData("train.csv")
 
 d = 0
 
-Xd, Yd = convertLinear(X, Y, d)
+Xd, Yd = convertLinear(X, Y, d, (d+1)%10)
 
-w, b = train(Xd, Yd, "linear", 1, 0)
+# w, b = train(Xd, Yd, "linear", 1, 0)
 
-print "accuracy (Linear Kernel) = ", test(w, b, "test.csv")
+# print "accuracy (Linear Kernel) = ", test(w, b, "test.csv")
 
 w, b = train(Xd, Yd, "gaussian", 1, 0.005)
 
