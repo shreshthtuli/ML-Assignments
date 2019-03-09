be5a9958c868de3ca40b66e479d8ab9f89009ad9
Shreshth Tuli <shreshthtuli@gmail.com>
2019-03-09 17:01:39 +0530

    Added libsvm

--- Support Vector Machine/Support_Vector_Machine.py
+++ Support Vector Machine/Support_Vector_Machine.py
@@ -12,7 +12,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cvxpy as cvx
 import math
-# import svmutil as svm
+from svmutil import *
 
 def parseData(filename):
     read = np.matrix([map(float, line.strip().split(',')) for line in open(filename)])
@@ -33,8 +33,7 @@ def savefig(x, name="Image"):
     plt.savefig(name)
 
 def linear_kernel(X, Y):
-	A = np.multiply(X, Y)
-	return A*A.T
+	return np.multiply(X, Y)*np.multiply(X, Y).T
 
 def gaussian_kernel(X, Y, gamma):
 	M = [map(float, [0]) * Y.shape[0] for _ in xrange(Y.shape[0])]
@@ -49,6 +48,18 @@ def weight_vector(X, Y, a):
 def intercept(x, y, w):
 	return y - w*(x.reshape(1,784).T)
 
+def listify(X, Y):
+    retx = []
+    rety = []
+    for i in xrange(Y.shape[0]):
+        rety.append(int(Y.item(i)))
+    for i in xrange(X.shape[0]):
+        param = []
+        for j in xrange(X.shape[1]):
+            param.append(X.item(i,j))
+        retx.append(param)
+    return retx, rety
+
 def train(X, Y, kernel_type, C=1, gamma=0.05):
     alpha = cvx.Variable((Y.shape[0], 1)) # Variable for optimization
     Q = linear_kernel(X, Y) if kernel_type == "linear" else gaussian_kernel(X, Y, gamma) # Kernel
@@ -56,15 +67,16 @@ def train(X, Y, kernel_type, C=1, gamma=0.05):
     constraints = [alpha >= 0, alpha <= C, alpha.T*Y == 0] # Constraints
         
     cvx.Problem(objective, constraints).solve() 
-    print alpha.value
+    # print alpha.value
     index = np.zeros((alpha.value.size, 1)) # indentify support vectors
-    sv = 0
+    sv = 0; count = 0
     for i in xrange(alpha.size):
         index[i,0] = alpha[i].value
         if alpha[i].value > 0.1 and alpha[i].value <= 1:
-            print i; sv = i
+            # print i
+            sv = i; count += 1
             # savefig(X[i].reshape(1, 784), "./sv/supportvector"+str(i)+"y"+str(Y[i])+".png")
-        
+    print "Num support vectors : ", count
     w = weight_vector(X, Y, index)
     b = intercept(X[sv], Y[sv], w)
     return w, b
@@ -79,28 +91,29 @@ def test(w, b, d, e, filename):
         clsfy = e if val >= 0 else d
         if clsfy == Y1.item(i):
             correct += 1
-        else:
-            savefig(X1[i].reshape(1, X.shape[1]), "./wrong/wrong"+str(total)+"a"+str(int(Y1.item(i)))+"p"+str(int(clsfy))+".png")
+        # else:
+        #     savefig(X1[i].reshape(1, X.shape[1]), "./wrong/wrong"+str(total)+"a"+str(int(Y1.item(i)))+"p"+str(int(clsfy))+".png")
         total += 1
     
     return float(correct) / float(total)
 
-def train_multiclass(X, Y, kernel_type, C=1, gamma=0.05):
-    w = np.empty((10, 10))
+def train_multiclass(X, Y, kernel_type, param):
+    w = np.empty((10, 10),dtype=object)
     b = np.empty((10, 10))
     for i in range(10):
         for j in range(i+1,10):
             Xd, Yd = convertLinear(X, Y, i, j)
-            w[i][j], b[i][j] = train(X, Y, kernel_type, C, gamma)
+            w[i][j], b[i][j] = train(Xd, Yd, kernel_type, C, gamma)
+            print("trained ("+str(i)+", "+str(j)+")")
     return w, b
 
-def classify(w, b, x):
+def classify(svm_model, x):
     wins = np.zeros(10)
     for i in range(10):
         for j in range(i+1, 10):
-            val = float(w*(x.reshape(1, 784).T)) + b
-            clsfy = j if val >= 0 else x
-            wins[clsdfy] += 1
+            svm_predict()
+            clsfy = j if val >= 0 else i
+            wins[clsfy] += 1
     return wins.argmax()
 
 def test_multiclass(w, b, filename):
@@ -111,24 +124,52 @@ def test_multiclass(w, b, filename):
         clsfy = classify(w, b, X1[i])
         if clsfy == Y1.item(i):
             correct += 1
-        else:
-            savefig(X1[i].reshape(1, X.shape[1]), "./wrong/wrong"+str(total)+"a"+str(int(Y1.item(i)))+"p"+str(int(clsfy))+".png")
+        # else:
+        #     savefig(X1[i].reshape(1, X.shape[1]), "./wrong/wrong"+str(total)+"a"+str(int(Y1.item(i)))+"p"+str(int(clsfy))+".png")
         total += 1
     
     return float(correct) / float(total)
 
 
 # Read data from file
-X, Y = parseData("train.csv")
+X, Y = parseData("train2.csv")
+print("Data parse complete...")
 
 d = 0
 
 Xd, Yd = convertLinear(X, Y, d, (d+1)%10)
 
-# w, b = train(Xd, Yd, "linear", 1, 0)
+w, b = train(Xd, Yd, "linear", 1, 0)
 
-# print "accuracy (Linear Kernel) = ", test(w, b, d, (d+1)%10, "test.csv")
+print "Accuracy (Linear Kernel) = ", test(w, b, d, (d+1)%10, "test2.csv")
 
 w, b = train(Xd, Yd, "gaussian", 1, 0.05)
 
-print "accuracy (Gaussian Kernel) = ", test(w, b, d, (d+1)%10, "test.csv")
+print "Accuracy (Gaussian Kernel) = ", test(w, b, d, (d+1)%10, "test2.csv")
+
+# w, b = train_multiclass(X, Y, "linear", 1, 0)
+
+# acc = test_multiclass(w, b, "test2.csv")
+
+# print "Multiclass Accuracy (Linear Kernel) = ", acc
+
+train_data, train_labels = listify(Xd, Yd)
+
+X1, Y1 = convertLinear(X, Y, d, (d+1)%10, True)
+test_data, test_labels = listify(X1, Y1)
+
+# Linear SVM Model
+# model = svm.svm_train(train_labels, train_data,'-t 0 -c 1')
+# svm.svm_predict(test_labels, test_data, model)
+
+# Gaussian SVM Model
+prob = svm_problem(test_labels, test_data)
+param = svm_parameter('-g 0.05 -c 4')
+param.C = 1
+
+model = svm_train(prob, param)
+[predicted_label, accuracy, decision_values] = svm_predict(test_labels, test_data, model)
+# print predicted_label
+print param.weight
+print param
+
