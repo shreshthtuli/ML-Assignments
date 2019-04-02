dc992618162a7f31b00126c08deeb9c512867ac3
Shreshth Tuli <shreshthtuli@gmail.com>
2019-04-02 21:31:26 +0530

    bug fixes

--- Decision Trees/Decision_Trees.py
+++ Decision Trees/Decision_Trees.py
@@ -10,8 +10,10 @@ Machine Learning Model : Decision Trees
 import numpy as np
 np.set_printoptions(threshold=np.inf)
 
-MAX_NODES = 100
+MAX_NODES = 100000
 NODES = 0
+
+TreeNodes = []
 
 def parseData(filename):
     file = open(filename, "r")
@@ -27,9 +29,12 @@ def preProcessData(X):
             X[j,i] = 1 if X[j,i] >= median else 0 # Continuous to binary
     for j in range(X.shape[0]):
         X[j,1] = X[j,1] - 1 # Binary 1,2 to 0,1
+    for i in range(5, 11):
+        for j in range(X.shape[0]):
+            X[j,i] = X[j,i] + 2
 
-def log2(val):
-    return 0 if val == 0 else np.log2(val)
+def plogp(num, den):
+    return 0 if num == 0 else (num/den)*np.log2(num/den)
 
 def getEntropy(D, attr, val):
     prob = 0.0
@@ -41,8 +46,8 @@ def getEntropy(D, attr, val):
             ones = ones + 1.0 if D[i,-1] == 1 else ones
             zeros = zeros + 1.0 if D[i,-1] == 0 else zeros
     prob = prob / D.shape[0]
-    entropy = - (ones/D.shape[0])*log2(ones/D.shape[0]) \
-              - (zeros/D.shape[0])*log2(zeros/D.shape[0])
+    entropy = - plogp(ones, zeros+ones) \
+              - plogp(zeros, zeros+ones)
     return prob, entropy
 
 def values(attribute):
@@ -52,7 +57,7 @@ def values(attribute):
     elif attribute == 3: # marital status
         values = range(4)
     elif attribute in range(5, 11): # Repayment status
-        values = range(-2, 10)
+        values = range(12)
     else: # Binary or conitunuous
         values = range(2)
     return values
@@ -72,12 +77,14 @@ class Node:
         self.data = Dat
         self.attr = None
         self.children = []
+        self.pruned = False
+        self.entropy = 0
         self.zeros = 0
         self.ones = 0
-        self.entropy = 0
         self.prediction = None
         NODES += 1
         self.isLeaf = True if NODES > MAX_NODES else False
+        TreeNodes.append(self)
         self.findAttribute()
     
     def information(self, attribute):
@@ -85,7 +92,6 @@ class Node:
         for value in values(attribute):
             prob, entr = getEntropy(self.data, attribute, value)
             childEntropy += prob * entr
-
         return self.entropy - childEntropy
 
     def predict(self):
@@ -93,6 +99,8 @@ class Node:
         except: pass
         self.zeros = self.data.shape[0] - self.ones if self.data.shape[1] > 0 else 0
         self.prediction = 1 if self.ones > self.zeros else 0
+        self.entropy = - plogp(self.ones, self.data.shape[0]) \
+                  - plogp(self.zeros, self.data.shape[0])
 
     def split(self):
         print "Splitting on attribute", self.attr, "into", len(values(self.attr)), "children"
@@ -100,21 +108,22 @@ class Node:
             self.children.append(Node(self, extract(self.data, self.attr, value)))
     
     def findAttribute(self):
-        self.predict()
-        self.entropy = - (self.ones/self.data.shape[0])*log2(self.ones/self.data.shape[0]) \
-                  - (self.zeros/self.data.shape[0])*log2(self.zeros/self.data.shape[0])
+        self.predict()     
         informationArray = np.array([self.information(a) for a in range(23)])
-        # print self.entropy, np.max(informationArray), self.zeros, self.ones
-        if np.max(informationArray) > 0:
+        # print informationArray, informationArray.argmax()
+        # exit(0)
+        if np.max(informationArray) > 0.001 and NODES < MAX_NODES:
             self.attr = informationArray.argmax()
         else:
-            self.isLeaf = True; # print "Leaf"
+            self.isLeaf = True;
         if not self.isLeaf:
             self.split()
         
     def test(self, dat):
-        if self.isLeaf or (self.children[dat[self.attr]].zeros == 0 and self.children[dat[self.attr]].ones == 0):
+        if self.isLeaf:
             return self.prediction
+        elif self.pruned:
+            return self.parent.prediction
         else:
             return self.children[dat[self.attr]].test(dat)
 
@@ -128,7 +137,8 @@ def Test(tree, filename):
         pred = tree.test(TestData[i])
         actual = TestData[i][-1]
         correct = correct + 1 if pred == actual else correct
-    print correct, len(TestData)
+    print "Accuracy = ", 100*correct/(len(TestData)+0.0)
+    print "Nodes = ", NODES
         
 
 Data = parseData('credit-cards.train.csv')
@@ -137,4 +147,6 @@ preProcessData(Data)
 Tree = Node(None, Data)
 print Tree.prediction, Tree.zeros, Tree.ones, Tree.data.shape[0], Tree.attr
 
+Test(Tree, "credit-cards.train.csv")
+Test(Tree, "credit-cards.val.csv")
 Test(Tree, "credit-cards.test.csv")
