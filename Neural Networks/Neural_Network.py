260566c514055d13c8628c369acc8711f778723c
Shreshth Tuli <shreshthtuli@gmail.com>
2019-04-06 17:45:00 +0530

    working

--- Neural Networks/Neural_Network.py
+++ Neural Networks/Neural_Network.py
@@ -65,7 +65,7 @@ class NeuralNet():
         self.layers.append(Layer(-1,in_size+1))
         for i in range(1,len(layers)):
             self.layers.append(Layer(layers[i-1]+1,layers[i]+1)); print layers[i-1]+1, layers[i]
-        self.layers.append(Layer(layers[-1]+1, 11))
+        self.layers.append(Layer(layers[-1]+1, out_size+1))
 
     def forward(self,x):
         self.layers[0].out = np.concatenate((np.ones((x.shape[0],1)),x),axis=1)
@@ -73,22 +73,47 @@ class NeuralNet():
             self.layers[idx].out = activation(np.dot(self.layers[idx-1].out,self.layers[idx].w))
             self.layers[idx].out = np.concatenate((np.ones((self.layers[idx].out.shape[0],1)),self.layers[idx].out),axis=1)
         self.layers[-1].out = activation(np.dot(self.layers[-2].out,self.layers[-1].w))
-        return activation(self.layers[-1].out)
+        return self.layers[-1].out
     
     def backprop(self, y, eta):
         for idx, layer in enumerate(self.layers[::-1]):
             if(idx == 0):
-                layer.delta = -1 * (y - layer.out) * layer.out.reshape(10,1) * (1 - layer.out)
-                # print layer.delta, layer.delta.shape, layer.w[1:,:].shape
+                layer.delta = -1 * np.array(y - layer.out) * np.array(layer.out) * np.array(1 - layer.out)
+                # print "Last layer:", y, "\n",  layer.out, "\n", -layer.delta
             else:
                 next_layer = self.layers[-idx]
-                layer.delta = np.einsum('ij,kj->ki',next_layer.w[1:,:],next_layer.delta) * layer.out[:,1:].reshape(layer.out[:,1:].shape[1],1) * (1 - layer.out[:,1:])
+                layer.delta = np.einsum('ij,kj->ki',next_layer.w[1:,:],next_layer.delta) * np.array(layer.out[:,1:]) * np.array(1 - layer.out[:,1:])
         for idx in range(1,len(self.layers)):
             self.layers[idx].w -= eta * np.einsum('ij,ik->jk',self.layers[idx-1].out,self.layers[idx].delta)
 
     def predict(self, x):
         return self.forward(x).argmax()
     
+    def fit(self, x_train, y_train, eta, epochs, batchsize=1):
+        batches = int(x_train.shape[0]/batchsize)
+        costArr = []; totalCost = 0
+        for e in range(epochs):
+            totalCost = 0
+            for i in range(batches):
+                self.forward(x_train[i*batchsize:(i+1)*batchsize])
+                self.backprop(y_train[i*batchsize:(i+1)*batchsize], eta)
+                cost = (np.linalg.norm(y_train[i*batchsize:(i+1)*batchsize] - self.layers[-1].out)**2).sum()/(2*batchsize)
+                totalCost += cost
+                # print "Cost: ", cost; 
+                # print x_train[i], self.forward(x_train[i]), self.predict(x_train[i]), y_train[i]
+            print "Cost:", totalCost, "Epoch:", e; 
+            if totalCost < 10: break;
+            costArr.append(totalCost)
+        return costArr
+    
+    def accuracy(self, x_test, y_test):
+        correct = 0
+        for i in range(x_test.shape[0]):
+            pred = self.pred(x_test[i])
+            actual = y_test[i]
+            if pred == actual:
+                count += 1
+        return 100*float(correct)/x_test.shape[0]
 
 # Preprocess files
 
@@ -102,19 +127,28 @@ if argv[1] == '0':
 # Neural Network
 
 Data = parseData("train.data")
+testData = parseData("test.data")
 
-# x_train = Data[:,:-1]
-# y_train = Data[:,-1]
+# Simple network
+# x_train = np.matrix([[1,1],[0,1],[1,0],[0,0]])
+# y_train = np.matrix([[1,0],[0,1],[0,1],[1,0]])
 
-x_train = np.matrix([1,2,3,4,5])
+# nn = NeuralNet(x_train.shape[1], 2, [3,3])
 
-nn = NeuralNet(x_train.shape[1], 10, [5, 10])
+# nn.fit(x_train, y_train, 0.5, 20000, 1)
 
-print nn.forward(x_train), nn.predict(x_train)
+# Real network
+x_train = Data[:,:-10]
+y_train = Data[:,-10:]
 
-y_train = np.matrix([0,1,0,0,0,0,0,0,0,0])
+x_test = testData[:,:-10]
+y_test = testData[:,-10:]
 
-for i in range(50):
-    nn.backprop(y_train, 0.5)
-    print nn.forward(x_train), nn.predict(x_train)
-    print
+nn = NeuralNet(x_train.shape[1], 10, [20])
+graph = nn.fit(x_train, y_train, 0.5, 100, 1)
+plt.plot(graph)
+plt.savefig("Cost with time"+'.png')
+print nn.accuracy(x_train, y_train)
+print nn.accuracy(x_test, y_test)
+
+
